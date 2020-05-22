58e225aa1b3f187738487052c88afe0f4a12ea58
Nevena Cukucan <ljubicpantic@gmail.com>
2020-05-22 13:08:24 -0400

    added tests to ProductPageTest

--- src/main/java/pageObjects/ProductPage.java
+++ src/main/java/pageObjects/ProductPage.java
@@ -7,57 +7,85 @@ import com.testautomationguru.ocular.snapshot.Snap;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
+import org.openqa.selenium.support.PageFactory;
 
 import java.nio.file.Path;
 import java.nio.file.Paths;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Map;
-import java.util.Random;
 
 @Snap("productPageSnap.png")
 public class ProductPage extends Page {
+
     private By sizeDropdown = By.id("pa_size");
+    private By colourDropdown = By.id("pa_color");
     private By productFrame = By.cssSelector("div.single-product-content");
     private By productTitle = By.cssSelector("h1.product_title");
     private By relatedProductContainer = By.cssSelector("div.noo-product-item");
     private By productContent = By.cssSelector("div.single-product-content");
-    private By galleryImages = By.cssSelector("div.noo-woo-thumbnails__slide");
+    //private By galleryImages = By.cssSelector("div.noo-woo-thumbnails__slide");
     private By originalPrice = By.cssSelector("p.price del");
     private By promotionalPrice = By.cssSelector("p.price ins");
+    private By cartQuantity = By.cssSelector("span.cart-item span.cart-name-and-total");
+    private By cartPrice = By.cssSelector("span.cart-item span.amount");
 
+    @FindBy(css = "button.single_add_to_cart_button")
+    private WebElement cartButton;
+
+    @FindBy(css = "div.noo-woo-thumbnails__slide")
+    private List<WebElement> thumbnails;
+
+    @FindBy(css = "table.variations")
+    private WebElement colourAndSizeDropdowns;
 
     public ProductPage(WebDriver driver) {
         super(driver);
-        //PageFactory.initElements(driver, this);
+        PageFactory.initElements(driver, this);
     }
 
-    public void selectSizeFromDropdown(String text) {
-        scrollUntilElement(getWebElement(productFrame, "PRODUCT FRAME"));
-        selectByText(sizeDropdown, text, "SIZE");
+    /**
+     * loop through all thumbnails and click on each of them
+     * get a thumbnail name and compare it with big image name
+     * @return boolean - do names match
+     */
+//    @FindBy(css = )
+//    private WebElement bigImage;
+//
+//    public List<Boolean> isEveryThumbnailEnlargedAfterClick(){
+//        int i = 1;
+//        List<Boolean> titleMatchingResults = new ArrayList<Boolean>();
+//        for(WebElement thumbnail : thumbnails){
+//            clickOnElement(thumbnail, "THUMBNAIL #" + i);
+//            boolean titlesMatch = thumbnail.getAttribute("title").equals(bigImage.getAttribute("title"));
+//            titleMatchingResults.add(titlesMatch);
+//            i++;
+//        }
+//        return titleMatchingResults;
+//    }
+
+    public boolean isCartButtonEnabled(){
+        scrollToProductSection();
+        return !cartButton.getAttribute("class").contains("disabled");
     }
 
-    public Boolean isSelectedOptionEqualTo(String value) {
-        return createSelectElement(sizeDropdown).getAllSelectedOptions()
-                .get(0)
-                .getText()
-                .equals(value);
+    public void addProductToCart(){
+        selectColour();
+        selectSize();
+        clickOnElement(cartButton, "ADD TO CART BUTTON");
     }
 
-    public int getNumberOfSizeOptions() {
+    public void selectColour() {
+        createSelectElement(colourDropdown).selectByIndex(1);
+    }
+
+    public void selectSize() {
+        createSelectElement(sizeDropdown).selectByIndex(1);
+    }
+
+    public int getNumberOfOptionsForSize() {
         return createSelectElement(sizeDropdown).getOptions().size();
-    }
-
-    public String getRandomOption() {
-        Random random = new Random();
-        int randomIndex = random.nextInt(3);
-        scrollUntilElement(getWebElement(sizeDropdown, "SIZE DROPDOWN"));
-        return createSelectElement(sizeDropdown).getOptions().get(randomIndex).getText();
-    }
-
-
-    public String getProductPageTitle() {
-        return getPageTitle();
     }
 
     public boolean isAt() {
@@ -87,6 +115,7 @@ public class ProductPage extends Page {
         return result.isEqualsImages();
     }
 
+
     public List<Boolean> toggleThroughGalleryImagesAndCompareSS() {
         /**
          *  fetch the list of all gallery photos
@@ -99,10 +128,8 @@ public class ProductPage extends Page {
 
         List<Boolean> areImagesCorrectlyDisplayed = new ArrayList<Boolean>();
         scrollToProductSection();
-        List<WebElement> imgs = driver.findElements(galleryImages);
         int i = 1;
-
-        for (WebElement img : imgs) {
+        for (WebElement img : thumbnails) {
             String filename = fileNames.get("img#" + i);
             clickOnElement(img, "IMAGE #" + i);
             boolean isCorrectlyDisplayed = verifyImage(filename);
@@ -125,6 +152,20 @@ public class ProductPage extends Page {
         return getPrice(originalPrice) > getPrice(promotionalPrice);
     }
 
+    public List<Boolean> areVariationsDisplayedAnEnabled() {
+        return List.of(getWebElement(sizeDropdown, "SIZE DROPDOWN").isEnabled(),
+                                    getWebElement(colourDropdown, "COLOUR DROPDOWN").isEnabled(),
+                                    colourAndSizeDropdowns.isDisplayed());
+        }
+
+    public int checkCartQuantity() {
+        String titleWithQuantity = driver.findElement(cartQuantity).getText().replace("Cart(", "").replace(")","");
+        return Integer.parseInt(titleWithQuantity);
+    }
+
+    public double checkCartPrice(){
+        return getPrice(cartPrice);
+    }
 
     public class RelatedProduct {
         private By productTitle = By.cssSelector("div h3");
--- src/test/java/testClasses/ProductPageTest.java
+++ src/test/java/testClasses/ProductPageTest.java
@@ -5,8 +5,7 @@ import pageObjects.ProductPage;
 
 import java.util.List;
 
-import static org.testng.Assert.assertEquals;
-import static org.testng.Assert.assertTrue;
+import static org.testng.Assert.*;
 import static testUtilities.TestListener.extentParallel;
 
 public class ProductPageTest extends BaseTest {
@@ -35,7 +34,7 @@ public class ProductPageTest extends BaseTest {
         assertEquals(imgsDisplayedOk, expectedBehavior);
     }
 
-    @Test
+    @Test(enabled=false)
     public void testIsPromotionalPriceLowerThanOriginal(){
         pp = new ProductPage(driver);
         extentParallel.get().info("Comparing the original and promotional price - promotional must be lower.");
@@ -49,11 +48,39 @@ public class ProductPageTest extends BaseTest {
     }
 
     @Test
-    public void testIsAddToCartButtonDisabled(){}
+    public void testAreColourAndSizeDropdownsAvailable(){
+        var expectedResults = List.of(true, true, true);
+        pp = new ProductPage(driver);
+        extentParallel.get().info("Verify if a table with Colour and Size dropdowns with their labels is displayed.");
+        var results = pp.areVariationsDisplayedAnEnabled();
+
+       assertEquals(results, expectedResults, "Some of the three conditions is false. Conditions: is table with variations displayed, are both dropdowns enabled");
+    }
 
     @Test
-    public void testAreColourAndSizeDropdownsAvailable(){}
+    public void testAddToCartButtonStates(){
+        pp = new ProductPage(driver);
+        boolean initialButtonState = pp.isCartButtonEnabled();
+        pp.selectColour();
+        pp.selectSize();
+        boolean afterButtonState = pp.isCartButtonEnabled();
+
+        assertFalse(initialButtonState);
+        assertTrue(afterButtonState);
+    }
 
     @Test
-    public void testCanUserAddProductToCart(){}
+    public void testIsAtLeastOneOptionAvailableInDropdowns(){
+
+    }
+
+
+    @Test
+    public void testCanUserAddProductToCart(){
+        pp = new ProductPage(driver);
+        pp.addProductToCart();
+        int addedProductNo = pp.checkCartQuantity();
+        double actualCartPrice = pp.checkCartPrice();
+
+    }
 }
